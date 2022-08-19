e3db57b27a5d28f32f0c8122085481977cf05657
andreykanava <66001105+andreykanava@users.noreply.github.com>
2022-08-19 21:40:56 +0300

    bug fix

--- main.py
+++ main.py
@@ -422,13 +422,14 @@ async def month_reset():
       
     for guild in client.guilds:
         for member in guild.members:
-          week[member.id] = {"voice": 0, "text": 0}
+          month[member.id] = {"voice": 0, "text": 0}
     await asyncio.sleep(2592000)
 
 async def day_reset():
-  for guild in client.guilds:
-      for member in guild.members:
-        week[member.id] = {"voice": 0, "text": 0}
-  await asyncio.sleep(86400)
+  while True:
+    for guild in client.guilds:
+        for member in guild.members:
+          day[member.id] = {"voice": 0, "text": 0}
+    await asyncio.sleep(86400)
 
 client.run('MTAwNTQ3NDM3MzIzMjI1MDk3MQ.GzM1u6.27UKqUdBdJdqCFrqZI22_ThHOkM2z-BZqDszvU')
